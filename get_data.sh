be3943f997b7cf19843f386603207f5190eee486
Adam <aghoumrassi@hotmail.co.uk>
2020-05-16 15:30:15 +0100

    added get_data.sh to pull preprocessed data and add to ./data

---
+++ get_data.sh
@@ -0,0 +1,3 @@
+wget -P data/ https://s3.amazonaws.com/code2seq/datasets/java-small-preprocessed.tar.gz
+7z e data/java-small-preprocessed.tar.gz -odata/
+7z e data/java-small-preprocessed.tar -odata/
