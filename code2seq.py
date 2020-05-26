5a40c35f127b4b461d4968e11e4510e0a8b80583
Vegar Andreas Bergum <vegar.andreas@gmail.com>
2020-05-26 16:45:03 +0100

    feat: add validation step to each epoch

--- code2seq.py
+++ code2seq.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 
 from model import Code2Seq, config
 from loader import Dictionaries, get_loaders
+from common import Common
 
 
 _mce = nn.CrossEntropyLoss(size_average=True, ignore_index=0)
@@ -16,12 +17,16 @@ def masked_cross_entropy(logits, target):
 
 def train(model, optimizer, loaders, epochs=10):
     train_loader = loaders['TRAIN_LOADER']
+    val_loader = loaders['VAL_LOADER']
 
     for epoch in range(epochs):
         with tqdm(total=len(train_loader), desc='TRAIN') as t:
             epoch_loss = 0.0
+            epoch_f1 = 0.0
             for i, batch in enumerate(train_loader):
                 start_leaf, ast_path, end_leaf, target, start_leaf_mask, end_leaf_mask, target_mask, context_mask, ast_path_lengths = batch
+                # Remove <SOS>
+                target = target[:,1:]
         
                 pred = model(*batch)
                 loss = masked_cross_entropy(pred.contiguous(), 
@@ -30,9 +35,34 @@ def train(model, optimizer, loaders, epochs=10):
                 loss.backward()
                 optimizer.step()
 
+                target = target.contiguous().data.cpu()[:,1:]
+                pred = pred.max(dim=-1)[1].data.cpu().t()
+
+                precision, recall, f1 = Common.get_scores(*model.get_evaluation(pred, target))
+
                 epoch_loss = (epoch_loss*i + loss.item()) / (i+1)
-                t.set_postfix(loss='{:05.3f}'.format(epoch_loss))
+                epoch_f1 = (epoch_f1*i + f1) / (i+1)
+                t.set_postfix(loss='{:05.3f}'.format(epoch_loss),
+                              f1='{:05.3f}'.format(epoch_f1),)
                 t.update()
+
+        with tqdm(total=len(val_loader), desc='VAL') as t:
+            for i, batch in enumerate(val_loader):
+                start_leaf, ast_path, end_leaf, target, start_leaf_mask, end_leaf_mask, target_mask, context_mask, ast_path_lengths = batch
+
+                pred = model(*batch)
+                loss = masked_cross_entropy(pred.contiguous(),
+                                            target.contiguous())
+
+                target = target.contiguous().data.cpu()[:,1:]
+                pred = pred.max(dim=-1)[1].data.cpu().t()
+
+                precision, recall, f1 = Common.get_scores(*model.get_evaluation(pred, target))
+
+                epoch_loss = (epoch_loss*i + loss.item()) / (i+1)
+                epoch_f1 = (epoch_f1*i + f1) / (i+1)
+                t.set_postfix(loss='{:05.3f}'.format(epoch_loss),
+                              f1='{:05.3f}'.format(epoch_f1),)
 
 
 if __name__=='__main__':
