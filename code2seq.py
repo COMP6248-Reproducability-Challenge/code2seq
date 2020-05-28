312585f5ec22dbcc8440d809d2e901624598554d
Adam <aghoumrassi@hotmail.co.uk>
2020-05-28 18:33:19 +0100

    Merged dataloader from Hukuda

--- code2seq.py
+++ code2seq.py
@@ -4,19 +4,16 @@ from torch import optim
 from torch.utils.data import DataLoader
 
 from tqdm import tqdm
-
+import h5py
 import time
 
 from model import Code2Seq, config
 from loader import Dictionaries, get_loaders
 from common import Common
+from dataset import C2SDataSet
 
-
-def train(model, optimizer, criterion, loaders, epochs=1):
-    train_loader = loaders['TRAIN_LOADER']
-    val_loader = loaders['VAL_LOADER']
-
-    ms = int(round(time.time()*1000))
+def train(model, optimizer, criterion, train_loader, val_loader, epochs=1):
+    ms = int(round(time.time() * 1000))
 
     for epoch in range(epochs):
         model.train(True)
@@ -48,10 +45,10 @@ def train(model, optimizer, criterion, loaders, epochs=1):
                 eval_ = model.get_evaluation(pred, target)
                 precision, recall, f1 = Common.get_scores(*eval_)
 
-                epoch_loss = (epoch_loss*i + loss.item()) / (i+1)
-                epoch_f1 = (epoch_f1*i + f1) / (i+1)
+                epoch_loss = (epoch_loss * i + loss.item()) / (i + 1)
+                epoch_f1 = (epoch_f1 * i + f1) / (i + 1)
                 t.set_postfix(loss='{:05.3f}'.format(epoch_loss),
-                              f1='{:05.3f}'.format(epoch_f1),)
+                              f1='{:05.3f}'.format(epoch_f1), )
                 t.update()
 
                 if i % 200 == 0:
@@ -92,21 +89,40 @@ def train(model, optimizer, criterion, loaders, epochs=1):
                 eval_ = model.get_evaluation(pred, target)
                 precision, recall, f1 = Common.get_scores(*eval_)
 
-                epoch_loss = (epoch_loss*i + loss.item()) / (i+1)
-                epoch_f1 = (epoch_f1*i + f1) / (i+1)
+                epoch_loss = (epoch_loss * i + loss.item()) / (i + 1)
+                epoch_f1 = (epoch_f1 * i + f1) / (i + 1)
                 t.set_postfix(loss='{:05.3f}'.format(epoch_loss),
-                              f1='{:05.3f}'.format(epoch_f1),)
+                              f1='{:05.3f}'.format(epoch_f1), )
                 t.update()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epoch", "-e", type=int, default=1,
+                        help="Number of examples in epoch")
+    parser.add_argument("--datapath",
+                        default="./data/java-small.dict.c2s",
+                        help="path of input data")
+    parser.add_argument("--trainpath",
+                        default=r"E:\University\DeepLearning\code2seq_pytorch_hukuda\train.h5",
+                        help="path of train data")
+    parser.add_argument("--validpath",
+                        default=r"E:\University\DeepLearning\code2seq_pytorch_hukuda\val.h5",
+                        help="path of valid data")
+    parser.add_argument("--trainnum", type=int, default=691974,
+                        help="size of train data")
+    parser.add_argument("--validnum", type=int, default=23844,
+                        help="size of valid data")
+    args = parser.parse_args()
+
     seed = 7
     torch.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     dicts = Dictionaries(config)
-    loaders = get_loaders(config, dicts, device)
 
     model = Code2Seq(dicts).to(device)
     model.train(True)
@@ -114,4 +130,20 @@ if __name__=='__main__':
     criterion = nn.CrossEntropyLoss(reduction='none')
     optimizer = optim.Adam(model.parameters())
 
-    train(model, optimizer, criterion, loaders, epochs=10)
+    train_h5 = h5py.File(args.trainpath, 'r')
+    val_h5 = h5py.File(args.validpath, 'r')
+
+    train_set = C2SDataSet(
+        config, train_h5, args.trainnum, dicts.subtoken_to_index,
+        dicts.node_to_index, dicts.target_to_index, device
+    )
+    val_set = C2SDataSet(
+        config, val_h5, args.validnum, dicts.subtoken_to_index,
+        dicts.node_to_index, dicts.target_to_index, device
+    )
+
+    train_loader = DataLoader(train_set, batch_size=config.BATCH_SIZE, shuffle=True)
+    val_loader = DataLoader(val_set, batch_size=config.BATCH_SIZE, shuffle=True)
+
+
+    train(model, optimizer, criterion, train_loader, val_loader, epochs=args.epoch)
